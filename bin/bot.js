670edf2ecdf6baa7b528ad58a517f3bc301294d5
Sophia Dengo <sdengo@gmail.com>
2016-03-11 16:40:36 -0500

    Bot returns events, users, and tasks.

--- bin/bot.js
+++ bin/bot.js
@@ -6,12 +6,13 @@ var token = process.env.BOT_API_KEY;
 var dbPath = 'data/norrisbot.db';
 var name = 'insightly';
 
-//console.log(dbPath, '\n', token);
-
 var norrisbot = new NorrisBot({
     token: token,
     dbPath: dbPath,
     name: name
 });
 
+
 norrisbot.run();
+
+
