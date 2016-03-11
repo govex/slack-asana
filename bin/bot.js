a3748898561e07cb5da8d1a2b03f09a7ce71f53c
Sophia Dengo <sdengo@gmail.com>
2016-03-11 16:44:40 -0500

    moved credentials into env variables.

--- bin/bot.js
+++ bin/bot.js
@@ -3,11 +3,13 @@
 var NorrisBot = require('../lib/norrisbot');
 
 var token = process.env.BOT_API_KEY;
+var username = process.env.USERNAME;
 var dbPath = 'data/norrisbot.db';
 var name = 'insightly';
 
 var norrisbot = new NorrisBot({
     token: token,
+    username: username,
     dbPath: dbPath,
     name: name
 });
