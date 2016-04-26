1c7246abfad9d1de53a9911c24ef5ea4dbd1f3c4
Sophia Dengo <sdengo@gmail.com>
2016-04-26 13:54:46 -0400

    changed filenames and function references from NorrisBot to InsightBot.

--- bin/bot.js
+++ bin/bot.js
@@ -4,13 +4,13 @@ var InsightBot = require('../lib/insightbot');
 
 var token = process.env.BOT_API_KEY;
 var username = process.env.USERNAME;
-var dbPath = 'data/norrisbot.db';
+//var dbPath = 'data/norrisbot.db';
 var name = 'insightly';
 
 var InsightBot = new InsightBot({
     token: token,
     username: username,
-    dbPath: dbPath,
+    //dbPath: dbPath,
     name: name
 });
 
